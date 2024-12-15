4d9590897b43a2d0d8616eabe3d5b2abd02b9997
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-12-15 01:40:20 +0000

    random card generator completed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let symbols = ["♦", "♥", "♠", "♣"];
+
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+let generateCard = () => {
+  let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let grabSymbols = document.getElementsByClassName("suit");
+  grabSymbols[0].innerHTML = randomSymbol;
+  grabSymbols[1].innerHTML = randomSymbol;
+  let grabNumber = document.getElementsByClassName("number")[0];
+  grabNumber.innerHTML = randomNumber;
+
+  if (randomSymbol === "♦" || randomSymbol === "♥") {
+    grabSymbols[0].style.color = "red";
+    grabSymbols[1].style.color = "red";
+  } else {
+    grabSymbols[0].style.color = "black";
+    grabSymbols[1].style.color = "black";
+  }
+};
+
+document.querySelector("button").addEventListener("click", generateCard);
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  setInterval(generateCard, 10000);
 };
